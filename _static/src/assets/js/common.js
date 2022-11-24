cfad90f133d2b5abf058fa317bf1c4ed3e1c2609
takayuki-takahashi-dic <ttkaws15@gmail.com>
2022-11-24 13:09:30 +0900

    【Others】ヘッダースクロール修正【0.5h】 【Others】フォームボタン色反転【0.5h】

--- _static/src/assets/js/common.js
+++ _static/src/assets/js/common.js
@@ -3,7 +3,7 @@ const $ = jQuery;
 
 require("./modules/subMenu");
 require("./modules/headerScroll");
-require("./modules/kvSlider");
 require("./modules/kvOverlay");
 require("./modules/parallaxImg");
-require("./modules/map");
+require("./modules/sectionTitle");
+require("./modules/triggerAnimation");
--- _static/src/assets/js/top.js
+++ _static/src/assets/js/top.js
@@ -1,4 +1,6 @@
 // require('./modules/microModal');
 require("./modules/fixedButton");
 require("./modules/homeSlider");
+require("./modules/homeCatch");
 require("./modules/kvSlider");
+require("./modules/loading");
